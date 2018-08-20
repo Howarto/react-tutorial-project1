254f2c232dbd844a3655304e283bc592f1c70927
Diego Lao Tebar <howarto@ymail.com>
2018-08-20 23:46:03 +0200

    Add press enter to input todo

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -6,7 +6,12 @@ export default class TodoForm extends Component {
       <div className="input-group input-group-lg">
         <input type="text" className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" ref={(node) => {
           this.input = node;
-        }} />
+        }} onKeyDown={(ev) => {
+          if (ev.key === 'Enter') {
+            this.props.onClick(this.input.value);
+            this.input.value = '';
+          }
+        }}/>
         <div className="input-group-btn">
           <button className="btn btn-outline-secondary" type="button" onClick={() => {
             this.props.onClick(this.input.value);
